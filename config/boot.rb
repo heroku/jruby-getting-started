3d0b5bdd8ae42a56c8e9ccaf2ceed4e4d795b0d7
Joe Kutner <jpkutner@gmail.com>
2015-04-20 13:31:25 -0500

    Added JCE exception logic to boot script

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,26 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+require 'java'
+
+java_import javax.crypto.Cipher
+java_import java.security.NoSuchAlgorithmException
+
+is_unlimited_jce = true
+begin
+  strength = Cipher.getMaxAllowedKeyLength("AES")
+  if strength <= 128
+    is_unlimited_jce = false
+  end
+rescue NoSuchAlgorithmException => nsae
+  is_unlimited_jce = false
+ensure
+  unless is_unlimited_jce
+    puts "WARNING: Disabling strong encryption because Unlimited JCE is not installed!"
+    security_class = java.lang.Class.for_name('javax.crypto.JceSecurity')
+    restricted_field = security_class.get_declared_field('isRestricted')
+    restricted_field.accessible = true
+    restricted_field.set nil, false
+  end
+end
